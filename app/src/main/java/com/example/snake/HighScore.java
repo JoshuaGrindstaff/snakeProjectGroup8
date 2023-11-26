e1dd62a11a678e8f63a77088d8b5cf4e90c619f1
Joshua Grindstaff <joshuagrindstaff@csus.edu>
2023-11-26 11:09:07 -0800

    Merged Tiaera's Changes with Josh's changes

--- app/src/main/java/com/example/snake/HighScore.java
+++ app/src/main/java/com/example/snake/HighScore.java
@@ -3,14 +3,14 @@ package com.example.snake;
 public class HighScore {
 }
 //public class HighScore implements Comparable<HighScore> {
-  //  private String playerName;
-    //private int score;
+//  private String playerName;
+//private int score;
 
-    // Constructors, getters, and setters
+// Constructors, getters, and setters
 
-    //@Override
-    //public int compareTo(HighScore other) {
-        // Implement comparison logic based on scores
-      //  return Integer.compare(other.getScore(), this.score);
-    //}
+//@Override
+//public int compareTo(HighScore other) {
+// Implement comparison logic based on scores
+//  return Integer.compare(other.getScore(), this.score);
+//}
 //}
