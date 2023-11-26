99c8bfeb9f9b72e1f59096cbf98a954a3f3520ae
Joshua Grindstaff <joshuagrindstaff@csus.edu>
2023-11-26 14:20:26 -0800

    Changed PowerUps to abstract
    Will be extend by real classes like spring

--- app/src/main/java/com/example/snake/PowerUps.java
+++ app/src/main/java/com/example/snake/PowerUps.java
@@ -2,59 +2,63 @@ package com.example.snake;
 
 import android.content.Context;
 import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
 import android.graphics.Paint;
 import android.graphics.Point;
 
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Random;
 
-public class PowerUps implements Collidable {
+public abstract class PowerUps implements Collidable {
     //Constant variable for number of powerups on screen before no more can spawn
     //location for this power up
-    Point location = new Point();
+    private Point location = new Point();
     private Point mSpawnRange;
     private int mSize;
-    private Bitmap mBitmapPower;
+    private Bitmap mBitmapPowerUp;
     private Random random;
-    private boolean active;
+    private Context context;
     public PowerUps(Context context, Point sr, int s)
     {
+        this.context = context;
         mSpawnRange = sr;
         mSize = s;
-
         location.x = -11;
-        //Load images into bitmaps
-        mBitmapPower = BitmapFactory.decodeResource(context.getResources(), R.drawable.spring);
-        //Resize images
-        mBitmapPower = Bitmap.createScaledBitmap(mBitmapPower,mSize,mSize,false);
+
         random = new Random();
     }
-    void spawn(){
+    public void spawn(){
         // Choose two random values and place the power up
             location.x = random.nextInt(mSpawnRange.x) + 1;
             location.y = random.nextInt(mSpawnRange.y - 1) + 1;
-            active = true;
+            //System.out.println("Spawn at \nX:" + location.x +"\nY:" + location.y);
     }
     // Let SnakeGame know where the power up is
     // SnakeGame can share this with the snake
+    public void despawn()
+    {
+        location.x = -11;
+    }
     @Override
     public Point getLocation()
     {
         return location;
     }
-    public boolean getActive()
+    protected void setBitmap(Bitmap mBitmapChild)
     {
-        return active;
+
+        mBitmapPowerUp = mBitmapChild;
+        mBitmapPowerUp = Bitmap.createScaledBitmap(mBitmapPowerUp,mSize,mSize,false);
     }
-    public void despawn()
-    {
-        active = false;
-        location.x = -11;
-    }
+    //abstract void draw(Canvas canvas, Paint paint);
     void draw(Canvas canvas, Paint paint){
-        canvas.drawBitmap(mBitmapPower,location.x * mSize,location.y * mSize,paint);
+        if(mBitmapPowerUp != null)
+        {
+            canvas.drawBitmap(mBitmapPowerUp,location.x * mSize,location.y * mSize,paint);
+            //System.out.println("Drawn at \nX:" + location.x +"\nY:" + location.y);
+        }
+        else
+            System.out.println("is null");
     }
+
+
 }
