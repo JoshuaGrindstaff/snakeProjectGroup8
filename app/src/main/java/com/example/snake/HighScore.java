e06cf370a5acec9e2fd9c3770abfd19e354dfec9
Joshua Grindstaff <joshuagrindstaff@csus.edu>
2023-11-26 11:49:34 -0800

    Moved code to HighScore
    Moved the some commented code from SnakeGame to HighScore since I think it will be put there

--- app/src/main/java/com/example/snake/HighScore.java
+++ app/src/main/java/com/example/snake/HighScore.java
@@ -13,4 +13,31 @@ public class HighScore {
 // Implement comparison logic based on scores
 //  return Integer.compare(other.getScore(), this.score);
 //}
+//*Tiaera: private void saveHighScores() {
+//    // Use SharedPreferences for simplicity (you can use a file or a database for more complex scenarios)
+//    SharedPreferences preferences = context.getSharedPreferences("HighScores", Context.MODE_PRIVATE);
+//    SharedPreferences.Editor editor = preferences.edit();
+//
+//    // Convert the list of HighScore objects to a JSON string
+//    Gson gson = new Gson();
+//    String json = gson.toJson(highScores);
+//
+//    // Save the JSON string
+//    editor.putString("highScores", json);
+//    editor.apply();
+//}
+//
+//private void loadHighScores() {
+//    SharedPreferences preferences = context.getSharedPreferences("HighScores", Context.MODE_PRIVATE);
+//    String json = preferences.getString("highScores", "");
+//
+//    // Convert the JSON string back to a list of HighScore objects
+//    Gson gson = new Gson();
+//    Type type = new TypeToken<List<HighScore>>(){}.getType();
+//    highScores = gson.fromJson(json, type);
+//
+//    if (highScores == null) {
+//        highScores = new ArrayList<>();
+//    }
+//}
 //}
